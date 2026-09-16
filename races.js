510eddb6380a4fed7b988ba154e31d57af219041
Thomazzo91 <Thomas.Landeke.Wilsmark@gmail.com>
2026-09-16 17:02:22 +0200

    Uppdatera lopp för alla

--- races.js
+++ races.js
@@ -1,7 +1,7 @@
 window.RACES = {
  "lopp1": {
   "name": "Helsingborg Marathon",
-  "rev": 49,
+  "rev": 50,
   "groups": [
    {
     "id": "soder",
@@ -13299,7 +13299,7 @@ window.RACES = {
  },
  "lopp2": {
   "name": "Malmö Marathon",
-  "rev": 52,
+  "rev": 53,
   "groups": [
    {
     "id": "startmal",
